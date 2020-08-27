afe1ca9e724f15d1bbb82bd17f3593ccf0ccea03
patri <patri@LAPTOP-HMSC0R2V>
2020-08-27 19:17:41 +0300

    In HospitalApp.java

--- src/hospitalapp/HospitalApp.java
+++ src/hospitalapp/HospitalApp.java
@@ -17,6 +17,7 @@ public class HospitalApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException {
+        
         // TODO code application logic here
         LocalDataBaseConnection.connectToLocalDB();
         LocalDataBaseConnection.populateAccountsHashMap();
